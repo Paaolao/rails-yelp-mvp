1df75a84acc6937d6b3c42580143c2066ab64a2c
Paola Olympio <olympiopaola@gmail.com>
2022-05-18 15:40:34 +0200

    yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 10 restaurants...'
+broc = Restaurant.create!(
+  name: 'Broc',
+  address: 'République',
+  category: 'french')
+moshi = Restaurant.create!(
+  name: 'Moshi Moshi',
+  address: 'Molinel',
+  category: 'japanese')
+oiseaux = Restaurant.create!(
+  name: 'Les Oiseaux',
+  address: 'République',
+  category: 'french')
+bellazza = Restaurant.create!(
+  name: 'La Bellazza',
+  address: 'Vieux Lille',
+  category: 'italian')
+pekin = Restaurant.create!(
+  name: 'Pekin',
+  address: 'Lomme',
+  category: 'chinese')
+
+puts 'Finished!'
